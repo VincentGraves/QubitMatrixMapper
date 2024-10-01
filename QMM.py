116cb6a5db97b1409cc1144421e15d549c0af3be
VincentGraves <32164697+VincentGraves@users.noreply.github.com>
2024-10-01 15:54:54 +0100

    Add Excitation Functions

--- QMM.py
+++ QMM.py
@@ -85,6 +85,46 @@ def classical_diag(hamil, ofpl):
         w = np.linalg.eigvals(hamil)
         return w
 
+def creatannih(ca, i):
+    #Get creation Pauli string.
+    pauli_oper = QubitOperator(((i, 'X')), 0.5)
+    if ca == 'creat':
+        pauli_oper += QubitOperator(((i, 'Y')), 0.5j)
+    elif ca == 'annih':
+        pauli_oper += QubitOperator(((i, 'Y')), -0.5j)
+    return pauli_oper
+
+def EF(params):
+    #Excitation Function approach
+    #initalise
+    hamil = QubitOperator()
+
+    #read input
+    int_readall = params['int_readall'] #Read integrals one at a time (0) or all together (1)
+    hamil_length = params['hamil_length']
+
+    num_qubits = hamil_length
+
+    #read all integrals
+    if int_readall != 0:
+        int_array = read_integrals(params, [hamil_length,hamil_length], int_readall)
+        if params['classic_eigen']:
+            print("Eigen values of input matrix (numpy): \n", classical_diag(int_array, -1))
+
+    for i in range(hamil_length):
+        for j in range(hamil_length):
+            #Get creation/annihalation operator
+            creat_op = creatannih('creat',i)
+            annih_op = creatannih('annih',j)
+
+            integral = int_array[i][j].tolist()
+            if integral == 0: continue
+
+            #Add to Hamiltonian
+            hamil += creat_op * annih_op * integral
+
+    return hamil
+
 def FQE(params):
 #Main FQE function
     #initalise
@@ -117,6 +157,7 @@ def FQE(params):
                 integral = read_integrals(params, [i, j], int_readall)
             else:
                 integral = int_array[i][j]
+            if integral == 0: continue
 
             #Work out parity
             parity = get_parity(initial, final)
@@ -138,21 +179,44 @@ def main():
     #Build FQE Hamiltonian
 
     print("Building FQE Hamiltonian")
-    q_ham = FQE(params)
 
-    print("Qubit Hamiltonian: ")
-    print("No. Qubits: ", count_qubits(q_ham), "No.terms: ", len(q_ham.terms))
-
-    if params['print_ham']:
-        save_operator(
-                operator = q_ham,
-                file_name = params['print_ham_tofile'],
-                data_directory = ".",
-                plain_text = True)
-
-    #Check qubit eigenvalues
-    if params['classic_eigen']:
-        print("Eigen values of qubit matrix (openfermion): \n", classical_diag(q_ham, 0))
+    if params['QEE_map']:
+        print("Using QEE Mapping:")
+        q_ham = FQE(params)
+    
+        print("Qubit Hamiltonian: ")
+        print("No. Qubits: ", count_qubits(q_ham), "No.terms: ", len(q_ham.terms))
+    
+        if params['print_ham']:
+            file_name = str(params['print_ham_tofile'] + ".QEE")
+            save_operator(
+                    operator = q_ham,
+                    file_name = file_name,
+                    data_directory = ".",
+                    plain_text = True)
+    
+        #Check qubit eigenvalues
+        if params['classic_eigen']:
+            print("Eigen values of qubit matrix (openfermion): \n", classical_diag(q_ham, 0))
+    
+    if params['EF_map']:
+        print("Using an EF Mapping:")
+        q_ham = EF(params)
+    
+        print("Qubit Hamiltonian: ")
+        print("No. Qubits: ", count_qubits(q_ham), "No.terms: ", len(q_ham.terms))
+    
+        if params['print_ham']:
+            file_name = str(params['print_ham_tofile'] + ".EF")
+            save_operator(
+                    operator = q_ham,
+                    file_name = file_name,
+                    data_directory = ".",
+                    plain_text = True)
+    
+        #Check qubit eigenvalues
+        if params['classic_eigen']:
+            print("Eigen values of qubit matrix (openfermion): \n", classical_diag(q_ham, 0))
 
     print("\nFinished.")
 
